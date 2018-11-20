eaf565e657b7b1bd48525f68d1e01e21781ef84b
essnyder <essnyder@bsu.edu>
2018-11-20 01:38:26 -0500

    Add files via upload
    Connected with db. 
    Established Message of the Day (MotD) process.

--- index.php
+++ index.php
@@ -10,10 +10,16 @@
 <body>
 	<div id="content">
     	<img src="images/logo.png" alt="logo">
-		<?php include_once("library/navBar.php"); ?>
-		<div class="titleBar">
-			Welcome Bob
-		</div>
+        <?php 
+			include_once("db_vars.php"); 
+			
+			$query = "SELECT message FROM tagline";
+			$result = mysql_query($query);
+			$tagline = mysql_result($result, 0); 
+
+			echo "<div class='titleBar'>" . $tagline . "</div>";
+		?>
+        
 		<div class="section">
 			<p>Hello and Welcome to the CS691 and TRS Project Website.</p>
 		</div>
